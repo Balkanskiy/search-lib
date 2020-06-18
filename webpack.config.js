c133e80e8f34ad2d84f2837e5bfd4b169c508b28
balkanskiy <balkanskiy.alexey@gmail.com>
2020-06-18 17:45:46 +0300

    Init modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+const pkg = require('./package.json');
+
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: pkg.main,
+    library: '',
+    libraryTarget: 'commonjs'
+  },
   module: {
     rules: [
       {
